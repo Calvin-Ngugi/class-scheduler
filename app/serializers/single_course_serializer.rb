db9ae8afc344699194ad80496447a7aeac92c12a
calvin <ncalvin67@gmail.com>
2023-01-16 16:08:13 +0300

    Data of single course correctly fetching

--- app/serializers/single_course_serializer.rb
+++ app/serializers/single_course_serializer.rb
@@ -1,4 +1,4 @@
 class SingleCourseSerializer < ActiveModel::Serializer
   attributes :id, :course_name, :description, :course_sessions
-  has_many :course_sessions, serializer: :CourseSessionSerializer, dependent: :destroy
+  has_many :course_sessions, serializer: CourseSessionSerializer, dependent: :destroy
 end
--- client/src/Components/Course/Header.js
+++ client/src/Components/Course/Header.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Header = ({course, sessions}) => {
   return (
     <div className='container'>
-        <h2>{course.course_name}</h2>
+        <h2 className='mt-3'>{course.course_name}</h2>
         <p>{course.description}</p>
-        <span className="session-count">There are {sessions ? sessions.length : 0} sessions in this program</span> 
+        <span className="session-count">There is {sessions ? sessions.length : 0} sessions in this program</span> 
     </div>
   )
 }
