bb437511dea901dc63f6b7dacc6c18ec1b338486
alexgicimo <gakurualexgicimo@gmail.com>
2023-01-16 13:40:19 +0000

    Display all course_sessions

--- client/src/Components/AllSessions.js
+++ client/src/Components/AllSessions.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-const AllSessions = () => {
+function AllSessions() {
+  const [sessions, setSessions] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(' /course_sessions')
+      .then(response => response.json())
+      .then(data => {
+        setSessions(data.sessions);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        console.error(error);
+        setIsLoading(false);
+      });
+  }, []);
+
   return (
-    <div>AllSessions</div>
-  )
+    <div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {sessions.map(session => (
+            <li key={session.id}>
+              <p>Session Date: {session.date}</p>
+              <p>Session Time: {session.time}</p>
+              <p>Session Duration: {session.duration}</p>
+              <p>Instructor Name: {session.instructorName}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default AllSessions
