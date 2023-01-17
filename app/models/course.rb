ae6b105ad12a720666b47f050992b42e590d799b
calvin <ncalvin67@gmail.com>
2023-01-17 18:27:21 +0300

    Course validations added

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,6 @@
 class Course < ApplicationRecord
     has_many :course_sessions
     has_many :comments, through: :course_sessions
+    validates :course_name, presence: true
+    validates :description, presence: true
 end
