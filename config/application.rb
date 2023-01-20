2ca340991732c62a98e5054aed22612ae99af1f1
calvin <ncalvin67@gmail.com>
2023-01-20 17:43:53 +0300

    Attempt at hosting

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module ClassScheduler
     
     config.load_defaults 7.0
 
+    config.hosts << "class-scheduler-production.up.railway.app"
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
